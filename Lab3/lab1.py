abb6444864ce3fbb7693e5b76f5b025df7a17e0c
jmalmonte <jmalmonte@myseneca.ca>
2022-10-15 00:58:07 -0400

    pushing to main branch

---
+++ Lab3/lab1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+try: 
+
+        age = int(input("Please enter your age: ")) 
+
+except ValueError: 
+
+        print("Please enter an int") 
+
+            age = int(input("Please enter an int for your age: ")) 
+
+            print(age)
