4e218bd674380a06ee1fff47b0024331e59f1052
jmalmonte <112995096+jmalmonte@users.noreply.github.com>
2022-10-14 23:47:23 -0400

    Update Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 
-#This stores the first argument as a string object
-name = sys.argv[1]
-#This stores the second argument as a string object
-age = sys.argv[2]
 
-print('Hi ' + name + ', you are '+str(age)+' years old.')
-print('The script and variables used are:')
-print('variable1 = '+name)
-print('variable2 = '+age)
+def f1(country, year):
+    print('You arrived in ' + country + ' in '+str(year)+'')
+    print('The script and variables used are:')
+    print('var1 = '+country)
+    print('var2 = '+year)
+
+if __name__ == '__main__':
+    #This stores the first argument as a string object
+    country = sys.argv[1]
+    #This stores the second argument as a string object
+    year = sys.argv[2]
+    f1(country, year)
+
